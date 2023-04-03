40505fe81046b27c8076e7161c6a2fd024d6ab75
Satrey <satrey.mail@gmail.com>
2023-04-03 17:46:50 +0500

    Добавлена форма регистрации нового пользователя.

--- movies/accounts/urls.py
+++ movies/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
-
+from .views import sign_up_account
 
 urlpatterns = [
-
+    path('sign_up_account/', sign_up_account, name='sign_up_account')
 ]
--- movies/accounts/views.py
+++ movies/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
-# Create your views here.
+
+def sign_up_account(request):
+    if not request.method == 'GET':
+        return render(request, 'accounts/sign_up_account.html', {'form': UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], request.POST['password'])
+            user.save()
+            login(request, user)
+            return redirect('home')
